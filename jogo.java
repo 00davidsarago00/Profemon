4390f2822e05fc7f61a63af88c6cfcde88a3b51e
Lucass <148711830+Lucas1234487@users.noreply.github.com>
2025-06-30 20:52:59 -0300

    Update jogo.java

--- jogo.java
+++ jogo.java
@@ -4,8 +4,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class jogo{
+public class jogo implements ActionListener {
     public static void main(String[] args) {
+        JFrame Principal = new JFrame("Jogo de Profemons");
+        Principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        PrincipalsetSize(800, 600);
+        PrincipalsetLayout(new BorderLayout());
+        
+        JPanel gamePanel = new JPanel();
+        JButton startButton = new JButton("Iniciar Jogo");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Jogo Iniciado!");
+            }
+        });
+
+        gamePanel.setBackground(Color.GREEN);
+        Principal.add(gamePanel, BorderLayout.CENTER);
        
+        Principal.setVisible(true);
+
+
     }
 }
