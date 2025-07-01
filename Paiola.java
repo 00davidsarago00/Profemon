cfae3feca927c8e051fe145b204ed95cbe8a4905
Lucass <148711830+Lucas1234487@users.noreply.github.com>
2025-07-01 05:41:51 -0300

    Create Paiola.java

---
+++ Paiola.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;  
+
+
+public class Paiola extends profemon {
+    public Paiola() {
+        this.nome = "Paiola";
+        this.tipo = "Programador";
+        this.ataque = 20;
+        this.vida = 100;
+        this.nivel = 1;
+        imagem = new ImageIcon("Sprints/" + nome + nivel + ".png");
+        this.vidamaxima = 100;
+        imagem = new ImageIcon(imagem.getImage().getScaledInstance(550, 550, Image.SCALE_SMOOTH));
+        this.label = new JLabel(imagem);
+        this.evolucao = 1;
+        this.velocidadedosataques[0] = 10; 
+        this.velocidadedosataques[1] = 15; 
+        this.velocidadedosataques[2] = 20;
+        this.velocidadedosataques[3] = 25;     
+    }
+    @Override
+    public void run() {
+        // Coloque aqui o que o Paiola deve fazer quando rodar em uma thread
+    }
+}
+
+    
