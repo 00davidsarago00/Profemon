c60d6751d7e4a521b3ea7ca6f154775b8791332f
Lucass <148711830+Lucas1234487@users.noreply.github.com>
2025-07-01 05:38:49 -0300

    Update inimigoPetista.java

--- inimigoPetista.java
+++ inimigoPetista.java
@@ -1,18 +1,20 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class inimigoPetista extends inimigos {
-    
-    public inimigoPetista(int posicaoX, int posicaoY) {
+    public inimigoPetista() {
         this.nome = "Petista";
         this.tipo = "EntraçonoRU";
         this.velocidade = 13;
-        this.posicaoX = posicaoX;
-        this.posicaoY = posicaoY;
         this.vida = 13;
         this.ataque = 13;
         this.defesa = 13;
-
         this.imagem = new ImageIcon("Sprints/petista.png");
         this.label = new JLabel(imagem);
-        
+        this.velocidadedosataques[0] = 10; 
+        this.velocidadedosataques[1] = 15; 
+        this.velocidadedosataques[2] = 20;
+        this.velocidadedosataques[3] = 25;
     }
-
 }
+
