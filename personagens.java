88fcabab32ff2e01c08129c20db6da9f37210c82
Lucass <148711830+Lucas1234487@users.noreply.github.com>
2025-07-03 18:04:52 -0300

    Update personagens.java

--- personagens.java
+++ personagens.java
@@ -13,18 +13,22 @@ public abstract class personagens {
     public int defesa;
     public int [] velocidadedosataques = new int[4];
     public int [] poderdosataques = new int[4];
+    
+    public String [] nomedosataques = new String[4];
 
 
-    ImageIcon imagemFrente;
+    ImageIcon imagemfrente;
     ImageIcon imagemderrotado;
     JLabel label;
     
     public abstract void derrotado();
 
     public void receberDano(int dano, personagens autor) {
-        this.vida -= dano / this.defesa;
+        this.vida -= dano;
         if (this.vida <= 0) {
             this.derrotado();
         }
     }
+
+    
 }
