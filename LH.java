893d464f418f4c50742df8603d5e4abecc6ae139
Lucass <148711830+Lucas1234487@users.noreply.github.com>
2025-07-03 18:08:36 -0300

    Update LH.java

--- LH.java
+++ LH.java
@@ -13,10 +13,12 @@ public class LH extends profemon {
         this.evolucao = 1;
         this.vidamaxima = 85;
         
-        imagemfrente = new ImageIcon("Sprints/" + nome + nivel + "Front" + ".png");
-        imagemcostas = new ImageIcon("Sprints/" + nome + nivel + "Back" + ".png");
-        imagemfrente = new ImageIcon(imagemfrente.getImage().getScaledInstance(550, 550, Image.SCALE_SMOOTH));
-        imagemcostas = new ImageIcon(imagemcostas.getImage().getScaledInstance(550, 550, Image.SCALE_SMOOTH));
+        imagemfrente = new ImageIcon("Sprints/" + nome + "1" + "Front" + ".png");
+        imagemcostas = new ImageIcon("Sprints/" + nome + "1" + "Back" + ".png");
+        imagemderrotado = new ImageIcon("Sprints/" + nome + "1" + "Back" + "BG.png");
+        imagemderrotado = new ImageIcon(imagemderrotado.getImage().getScaledInstance(200, 300, Image.SCALE_SMOOTH));
+        imagemfrente = new ImageIcon(imagemfrente.getImage().getScaledInstance(400, 600, Image.SCALE_SMOOTH));
+        imagemcostas = new ImageIcon(imagemcostas.getImage().getScaledInstance(400, 600, Image.SCALE_SMOOTH));
         
         this.label = new JLabel(imagemfrente);
         
@@ -30,6 +32,11 @@ public class LH extends profemon {
         this.poderdosataques[1] = 0;
         this.poderdosataques[2] = 80;
         this.poderdosataques[3] = 140;
+        
+        this.nomedosataques[0] = "De Morgan";
+        this.nomedosataques[1] = "Tautologia";
+        this.nomedosataques[2] = "Inferência Lógica";
+        this.nomedosataques[3] = "Prova por Contradição";
 
     }
     
